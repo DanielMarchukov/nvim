04d5cae39cf7b06e505e2249199fd7479f0515ae
Danielius Marciukovas <danielmarchukov@gmail.com>
2025-03-28 23:12:41 +0000

    Fix Mason configuration, go back to basics

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,42 +1,18 @@
 return {
   {
     "williamboman/mason.nvim",
-    dependencies = {
-      "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = {},
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "clangd",
+        "clang-format",
+        "cmakelang",
+        "gradle-language-server",
+        "jdtls",
+      },
     },
-    config = function()
-      local mason = require("mason")
-      local mason_tool_installer = require("mason-tool-installer")
-
-      -- enable mason and configure icons
-      mason.setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-        log_level = "info",
-        max_concurrent_installers = 4,
-      })
-
-      mason_tool_installer.setup({
-        ensure_installed = {
-          "prettier",
-          "prettierd",
-          -- "ktlint",
-          "eslint_d",
-          -- "google-java-format",
-          "htmlbeautifier",
-          "beautysh",
-          "buf",
-          "yamlfix",
-          "shellcheck",
-          "gopls",
-        },
-      })
-    end,
-  }
+  },
 }
-
