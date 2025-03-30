6e08dcbffe72e4e3027c010433d86ead406ea40a
Danielius Marciukovas <danielmarchukov@gmail.com>
2025-03-30 12:26:59 +0100

    Use LazyExtras for most of the configuration instead. This simplifies the initial setup, and I can override the defaults whenever

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,18 +1,20 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    dependencies = {},
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "clangd",
-        "clang-format",
-        "cmakelang",
-        "gradle-language-server",
-        "jdtls",
-      },
-    },
-  },
+  --   {
+  --     "williamboman/mason.nvim",
+  --     dependencies = {},
+  --     opts = {
+  --       ensure_installed = {
+  --         "stylua",
+  --         "shellcheck",
+  --         "shfmt",
+  --         "clangd",
+  --         "clang-format",
+  --         "cmakelang",
+  --         "gradle-language-server",
+  --         "jdtls",
+  --         "pyright",
+  --         "codelldb",
+  --       },
+  --     },
+  --   },
 }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -18,225 +18,226 @@ return {
       },
     },
   },
-  {
-    "mfussenegger/nvim-jdtls",
-  },
-  {
-    "stevearc/oil.nvim",
-    opts = {},
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-        delete_to_trash = true,
-        skip_confirm_for_simple_edits = true,
-        view_options = {
-          show_hidden = true,
-          natural_order = true,
-          is_always_hidden = function(name, _)
-            return name == ".." or name == ".git"
-          end,
-        },
-        float = {
-          padding = 2,
-          max_width = 90,
-          max_height = 0,
-        },
-        win_options = {
-          wrap = true,
-          winblend = 0,
-        },
-        keymaps = {
-          ["<C-c>"] = false,
-          ["q"] = "actions.close",
-        },
-      })
-    end,
-  },
-  { "nvim-treesitter/nvim-treesitter-textobjects" },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
+  -- {
+  --   "mfussenegger/nvim-jdtls",
+  -- },
+  -- {
+  --   "stevearc/oil.nvim",
+  --   opts = {},
+  --   dependencies = { "nvim-tree/nvim-web-devicons" },
+  --   config = function()
+  --     require("oil").setup({
+  --       default_file_explorer = true,
+  --       delete_to_trash = true,
+  --       skip_confirm_for_simple_edits = true,
+  --       view_options = {
+  --         show_hidden = true,
+  --         natural_order = true,
+  --         is_always_hidden = function(name, _)
+  --           return name == ".." or name == ".git"
+  --         end,
+  --       },
+  --       float = {
+  --         padding = 2,
+  --         max_width = 90,
+  --         max_height = 0,
+  --       },
+  --       win_options = {
+  --         wrap = true,
+  --         winblend = 0,
+  --       },
+  --       keymaps = {
+  --         ["<C-c>"] = false,
+  --         ["q"] = "actions.close",
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- { "nvim-treesitter/nvim-treesitter-textobjects" },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   build = ":TSUpdate",
+  --   config = function()
+  --     local configs = require("nvim-treesitter.configs")
 
-      configs.setup({
-        TSConfig = false,
-        ignore_install = {},
-        auto_install = true,
-        modules = {},
-        ensure_installed = {
-          "javascript",
-          "typescript",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "kotlin",
-          "jq",
-          "dockerfile",
-          "json",
-          "html",
-          "tsx",
-          "bash",
-          "markdown",
-          "java",
-        },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = "<C-CR>",
-            node_decremental = "<bs>",
-          },
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-          move = {
-            enable = true,
-            set_jumps = true, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]m"] = "@function.outer",
-              ["]]"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]M"] = "@function.outer",
-              ["]["] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[m"] = "@function.outer",
-              ["[["] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[M"] = "@function.outer",
-              ["[]"] = "@class.outer",
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ["<leader>p"] = "@parameter.inner",
-            },
-            swap_previous = {
-              ["<leader>ps"] = "@parameter.inner",
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "catppuccin/nvim",
-  },
-  {
-    "nvim-tree/nvim-web-devicons"
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "tokyonight",
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "diff", "diagnostics" },
-          lualine_c = { { "filename", path = 1 } },
-          lualine_x = {
-            { "fileformat", "filetype" },
-            {
-              require("noice").api.statusline.mode.get,
-              cond = require("noice").api.statusline.mode.has,
-              color = { fg = "#ff9e64" },
-            },
-          },
-          lualine_y = { "progress" },
-          lualine_z = { "location" },
-        },
-        extensions = { "fugitive", "quickfix", "fzf", "lazy", "mason", "nvim-dap-ui", "oil", "trouble" },
-      })
-    end,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({
-        on_attach = function(bufnr)
-          local gs = package.loaded.gitsigns
+  --     configs.setup({
+  --       TSConfig = false,
+  --       ignore_install = {},
+  --       auto_install = true,
+  --       modules = {},
+  --       ensure_installed = {
+  --         "javascript",
+  --         "typescript",
+  --         "lua",
+  --         "vim",
+  --         "vimdoc",
+  --         "query",
+  --         "kotlin",
+  --         "jq",
+  --         "dockerfile",
+  --         "json",
+  --         "html",
+  --         "tsx",
+  --         "bash",
+  --         "markdown",
+  --         "java",
+  --         "r",
+  --       },
+  --       sync_install = false,
+  --       highlight = { enable = true },
+  --       indent = { enable = true },
+  --       incremental_selection = {
+  --         enable = true,
+  --         keymaps = {
+  --           init_selection = "<C-space>",
+  --           node_incremental = "<C-space>",
+  --           scope_incremental = "<C-CR>",
+  --           node_decremental = "<bs>",
+  --         },
+  --       },
+  --       textobjects = {
+  --         select = {
+  --           enable = true,
+  --           lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+  --           keymaps = {
+  --             -- You can use the capture groups defined in textobjects.scm
+  --             ["aa"] = "@parameter.outer",
+  --             ["ia"] = "@parameter.inner",
+  --             ["af"] = "@function.outer",
+  --             ["if"] = "@function.inner",
+  --             ["ac"] = "@class.outer",
+  --             ["ic"] = "@class.inner",
+  --           },
+  --         },
+  --         move = {
+  --           enable = true,
+  --           set_jumps = true, -- whether to set jumps in the jumplist
+  --           goto_next_start = {
+  --             ["]m"] = "@function.outer",
+  --             ["]]"] = "@class.outer",
+  --           },
+  --           goto_next_end = {
+  --             ["]M"] = "@function.outer",
+  --             ["]["] = "@class.outer",
+  --           },
+  --           goto_previous_start = {
+  --             ["[m"] = "@function.outer",
+  --             ["[["] = "@class.outer",
+  --           },
+  --           goto_previous_end = {
+  --             ["[M"] = "@function.outer",
+  --             ["[]"] = "@class.outer",
+  --           },
+  --         },
+  --         swap = {
+  --           enable = true,
+  --           swap_next = {
+  --             ["<leader>p"] = "@parameter.inner",
+  --           },
+  --           swap_previous = {
+  --             ["<leader>ps"] = "@parameter.inner",
+  --           },
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "catppuccin/nvim",
+  -- },
+  -- {
+  --   "nvim-tree/nvim-web-devicons",
+  -- },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   dependencies = { "nvim-tree/nvim-web-devicons" },
+  --   config = function()
+  --     require("lualine").setup({
+  --       options = {
+  --         theme = "tokyonight",
+  --       },
+  --       sections = {
+  --         lualine_a = { "mode" },
+  --         lualine_b = { "diff", "diagnostics" },
+  --         lualine_c = { { "filename", path = 1 } },
+  --         lualine_x = {
+  --           { "fileformat", "filetype" },
+  --           {
+  --             require("noice").api.statusline.mode.get,
+  --             cond = require("noice").api.statusline.mode.has,
+  --             color = { fg = "#ff9e64" },
+  --           },
+  --         },
+  --         lualine_y = { "progress" },
+  --         lualine_z = { "location" },
+  --       },
+  --       extensions = { "fugitive", "quickfix", "fzf", "lazy", "mason", "nvim-dap-ui", "oil", "trouble" },
+  --     })
+  --   end,
+  -- },
+  -- {
+  --   "lewis6991/gitsigns.nvim",
+  --   config = function()
+  --     require("gitsigns").setup({
+  --       on_attach = function(bufnr)
+  --         local gs = package.loaded.gitsigns
 
-          local function map(mode, l, r, opts)
-            opts = opts or {}
-            opts.buffer = bufnr
-            vim.keymap.set(mode, l, r, opts)
-          end
+  --         local function map(mode, l, r, opts)
+  --           opts = opts or {}
+  --           opts.buffer = bufnr
+  --           vim.keymap.set(mode, l, r, opts)
+  --         end
 
-          -- Navigation
-          map("n", "]c", function()
-            if vim.wo.diff then
-              return "]c"
-            end
-            vim.schedule(function()
-              gs.next_hunk()
-            end)
-            return "<Ignore>"
-          end, { expr = true })
+  --         -- Navigation
+  --         map("n", "]c", function()
+  --           if vim.wo.diff then
+  --             return "]c"
+  --           end
+  --           vim.schedule(function()
+  --             gs.next_hunk()
+  --           end)
+  --           return "<Ignore>"
+  --         end, { expr = true })
 
-          map("n", "[c", function()
-            if vim.wo.diff then
-              return "[c"
-            end
-            vim.schedule(function()
-              gs.prev_hunk()
-            end)
-            return "<Ignore>"
-          end, { expr = true })
+  --         map("n", "[c", function()
+  --           if vim.wo.diff then
+  --             return "[c"
+  --           end
+  --           vim.schedule(function()
+  --             gs.prev_hunk()
+  --           end)
+  --           return "<Ignore>"
+  --         end, { expr = true })
 
-          -- Actions
-          map("n", "<leader>hs", gs.stage_hunk, { desc = "GitSigns state hunk" })
-          map("n", "<leader>hr", gs.reset_hunk, { desc = "GitSigns reset hunk" })
-          map("v", "<leader>hs", function()
-            gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end, { desc = "GitSigns stage_hunk" })
-          map("v", "<leader>hr", function()
-            gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-          end, { desc = "GitSigns reset_hunk" })
-          map("n", "<leader>hS", gs.stage_buffer, { desc = "GitSigns stage_buffer" })
-          map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "GitSigns undo_stage_hunk" })
-          map("n", "<leader>hR", gs.reset_buffer, { desc = "GitSigns reset_buffer" })
-          map("n", "<leader>hp", gs.preview_hunk, { desc = "GitSigns preview_hunk" })
-          map("n", "<leader>hb", function()
-            gs.blame_line({ full = true })
-          end, { desc = "GitSigns blame line" })
-          map("n", "<leader>htb", gs.toggle_current_line_blame, { desc = "GitSigns toggle blame" })
-          map("n", "<leader>hd", gs.diffthis, { desc = "GitSigns diffthis" })
-          map("n", "<leader>hD", function()
-            gs.diffthis("~")
-          end, { desc = "GitSigns diffthis" })
-          map("n", "<leader>htd", gs.toggle_deleted, { desc = "GitSigns toggle_deleted" })
+  --         -- Actions
+  --         map("n", "<leader>hs", gs.stage_hunk, { desc = "GitSigns state hunk" })
+  --         map("n", "<leader>hr", gs.reset_hunk, { desc = "GitSigns reset hunk" })
+  --         map("v", "<leader>hs", function()
+  --           gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+  --         end, { desc = "GitSigns stage_hunk" })
+  --         map("v", "<leader>hr", function()
+  --           gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+  --         end, { desc = "GitSigns reset_hunk" })
+  --         map("n", "<leader>hS", gs.stage_buffer, { desc = "GitSigns stage_buffer" })
+  --         map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "GitSigns undo_stage_hunk" })
+  --         map("n", "<leader>hR", gs.reset_buffer, { desc = "GitSigns reset_buffer" })
+  --         map("n", "<leader>hp", gs.preview_hunk, { desc = "GitSigns preview_hunk" })
+  --         map("n", "<leader>hb", function()
+  --           gs.blame_line({ full = true })
+  --         end, { desc = "GitSigns blame line" })
+  --         map("n", "<leader>htb", gs.toggle_current_line_blame, { desc = "GitSigns toggle blame" })
+  --         map("n", "<leader>hd", gs.diffthis, { desc = "GitSigns diffthis" })
+  --         map("n", "<leader>hD", function()
+  --           gs.diffthis("~")
+  --         end, { desc = "GitSigns diffthis" })
+  --         map("n", "<leader>htd", gs.toggle_deleted, { desc = "GitSigns toggle_deleted" })
 
-          -- Text object
-          map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns select hunk" })
-        end,
-      })
-    end,
-  },
+  --         -- Text object
+  --         map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns select hunk" })
+  --       end,
+  --     })
+  --   end,
+  -- },
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
@@ -263,8 +264,7 @@ return {
       colorscheme = "catppuccin-mocha",
     },
   },
-
-  -- override nvim-cmp and add cmp-emoji
+  -- override nvim-dap-ui and add cmp-emoji
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
@@ -272,16 +272,5 @@ return {
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
     end,
-  },
-
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {},
-    opts = {
-      servers = {
-        tsserver = {},
-        pyright = {},
-      },
-    },
   },
 }
